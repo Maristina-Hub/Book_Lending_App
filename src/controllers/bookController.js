27e9d05973ffd00ad9615f64dc226d4154b7c745
tonyfrenzy <tonyfrenzy@gmail.com>
2021-09-17 18:14:21 +0100

    update book controller

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,36 +1,39 @@
-    import { Book } from '../models/bookModel.js';
+import { Book } from '../models/bookModel.js';
 
-    export const BookController = {
-        createBook: async (req, res) => {
-        const { title, author, category, description, year } = req.body;
-        const role = req.user.role
+export const BookController = {
+    createBook: async (req, res) => {
+    const { title, author, category, description, year } = req.body;
+    const role = req.user.role
 
-        try {
-            if (!role || role !== 'admin') {
-            return res.status(401).json({ status: 'fail', message: 'unauthorized' });
-            }
-            if (!title || !description || !author || !category || !year) {
-            return res
-                .status(400)
-                .json({ status: 'fail', message: 'Please fill all fields' });
-            }
+    try {
+        if (!role || role !== 'admin') {
+        return res.status(401).json({ status: 'fail', message: 'unauthorized' });
+        }
+        if (!title || !description || !author || !category || !year) {
+        return res
+            .status(400)
+            .json({ status: 'fail', message: 'Please fill all fields' });
+        }
 
-            const newBook = new Book(req.body);
-            const book = await newBook.save();
-            if (!book) {
-                return res
-                .status(400)
-                .json({ status: 'fail', message: 'something went wrong' });
-            }
+        const newBook = new Book(req.body);
+        const book = await newBook.save();
+        if (!book) {
             return res
-                .status(200)
-                .json({ status: 'success', message: 'successful', data: { title, author, category, description, year } });
-            } catch (err) {
-            return res
-                .status(500)
-                .json({ status: 'fail', message: 'server err', err });
-            }
-        },
+            .status(400)
+            .json({ status: 'fail', message: 'something went wrong' });
+        }
+        return res
+            .status(200)
+            .json({ 
+                status: 'success', message: 'successful', data: book
+                // { title, author, category, description, year } 
+        });
+        } catch (err) {
+        return res
+            .status(500)
+            .json({ status: 'fail', message: 'server err', err });
+        }
+    },
 
     getBook: async (req, res) => {
         const PAGE_SIZE = 20;
@@ -66,7 +69,7 @@
         try {
         const book = await Book.findById(id)
         return res
-            .status(201)
+            .status(200)
             .json({ status: 'success', message: 'successful', data: book });
         } catch (err) {
         return res
@@ -113,20 +116,36 @@
     },
 
     deleteBook: async(req,res)=>{
-        const { id: _id } = req.params;
+        const bookId = req.params.id;
+        // const { bookId: _id } = req.params;
         const role = req.user.role
 
         if (!role || role !== 'admin') {
         return res.status(401).json({ status: 'fail', message: 'unauthorized' });
         }
         
-    try {
-        const savedBook = await Book.findByIdAndRemove(id);
-        res.json({msg: "Book deleted"})
-    } catch (error) {
-        res.status(400).send(error.reason={msg: "id not found"});
+        try {
+            // const savedBook = await Book.findByIdAndRemove(id);
+            // res.json({msg: "Book deleted"})
+
+            const deletedBook = await Book.findByIdAndRemove(bookId);
+    
+            if (deletedBook) {
+            return res
+                .status(200)
+                .json({
+                status: "success", message: "Book deleted"
+            });
+            }
+    
+            return res
+                .status(404)
+                .json({ status: 'fail', message: 'book not found' });
+                
+        } catch (error) {
+            res.status(400).send(error.reason={msg: "id not found"});
+        }
     }
-}
 }
 
 
